ba57bf23d5761605b7774528a89da3da34d8bf87
wahaha216 <wahaha216@outlook.com>
2024-06-13 20:24:45 +0800

    目前在QQ测试可用的版本

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,23 @@ import { FileInfo } from "./types";
 import { sizeFormat } from "./utils/sizeFormat";
 import {} from "@koishijs/plugin-logger";
 import {} from "@koishijs/plugin-http";
-import path from "path";
-import fs from "fs/promises";
-import { pathToFileURL } from "url";
+import {} from "koishi-plugin-adapter-onebot";
 
 export const name = "steam-workshop";
 
 export interface Config {
   autoRecognise?: boolean;
   askDownload?: boolean;
+  inputTimeout?: number;
 }
 
 export const Config: Schema<Config> = Schema.object({
-  autoRecognise: Schema.boolean()
-    .description("自动识别创意工坊链接")
-    .default(true),
-  askDownload: Schema.boolean().description("询问是否下载文件").default(true),
+  autoRecognise: Schema.boolean().default(true),
+  askDownload: Schema.boolean().default(true),
+  inputTimeout: Schema.number().default(10000).min(1000),
+}).i18n({
+  "zh-CN": require("./locales/zh-CN")._config,
+  "en-US": require("./locales/en-US")._config,
 });
 
 export const inject = {
@@ -27,63 +28,87 @@ export const inject = {
 
 export function apply(ctx: Context, config: Config) {
   // write your plugin here
+  ctx.i18n.define("en-US", require("./locales/en-US"));
+  ctx.i18n.define("zh-CN", require("./locales/zh-CN"));
+
   const regexp =
     /^https:\/\/steamcommunity.com\/sharedfiles\/filedetails\/\?id=\d+/;
   const logger = ctx.logger("wahaha216-steam-workshop");
-  ctx
-    .command("workshop <url:string>", "识别创意工坊物品链接")
-    .action(async ({ session }, url) => {
-      const id = session.messageId;
-      if (regexp.test(url)) {
-        const workId = url.split("id=")[1];
-        const res = await ctx.http
-          .post<FileInfo[]>(
-            "https://db.steamworkshopdownloader.io/prod/api/details/file",
-            `[${workId}]`,
-            { responseType: "json" }
-          )
-          .catch(logger.error);
-        if (res) {
-          const data = res[0];
-          await session.send([
-            h.quote(id),
-            h.text(`标题：${data.title}`),
-            h.text(`文件大小：${sizeFormat(data.file_size)}`),
-            h.text(`游戏：${data.app_name}`),
-            h.text(`描述：${data.file_description}`),
-            h.image(data.preview_url),
-          ]);
-          if (!config.askDownload) return;
-          await session.send([
-            h.quote(id),
+  ctx.command("workshop <url:string>").action(async ({ session }, url) => {
+    const id = session.messageId;
+    if (regexp.test(url)) {
+      const u = new URL(url);
+      const workId = u.searchParams.get("id");
+      const res = await ctx.http
+        .post<FileInfo[]>(
+          "https://db.steamworkshopdownloader.io/prod/api/details/file",
+          `[${workId}]`,
+          { responseType: "json" }
+        )
+        .catch(logger.error);
+
+      if (res) {
+        const data = res[0];
+        const fragment: h.Fragment = [
+          h.quote(id),
+          h.text(`${session.text(".title")}: ${data.title}\n`),
+          h.text(
+            `${session.text(".fileSize")}: ${sizeFormat(data.file_size)}\n`
+          ),
+          h.text(`${session.text(".game")}: ${data.app_name}\n`),
+          h.text(`${session.text(".description")}: ${data.file_description}\n`),
+          h.image(data.preview_url),
+        ];
+        if (config.askDownload) {
+          fragment.push(
+            h.text("=".repeat(20) + "\n"),
             h.text(
-              `是否下载：${data.title} ？（10秒内回复：是|y|yes/否|n|no）`
-            ),
-          ]);
-          const download = await session.prompt(10000);
-          if (!download) return [h.quote(id), h.text("输入超时")];
-          if (["是", "y", "yes"].includes(download.toLocaleLowerCase())) {
-            const file = await ctx.http.get(data.file_url, {
-              responseType: "arraybuffer",
-            });
-            const root = path.join(ctx.baseDir, "temp", name);
-            await fs.mkdir(root, { recursive: true });
-            const fileFullPath = `${root}/${data.filename}`;
-            await fs.writeFile(fileFullPath, Buffer.from(file));
-            await session.send([h.file(pathToFileURL(fileFullPath).href)]);
+              session.text(".ask_download", [
+                data.title,
+                config.inputTimeout / 1000,
+              ])
+            )
+          );
+        }
+        await session.send(fragment);
+
+        const download = await session.prompt(10000);
+        if (!download)
+          return [h.quote(id), h.text(session.text(".input_timeout"))];
+        if (["是", "y", "yes"].includes(download.toLocaleLowerCase())) {
+          // 因直接使用 h.file 无法上传文件，所以直接访问内部api
+          // 至少我测试时无法上传
+          if (session.bot.platform === "onebot") {
+            const path = await session.onebot.downloadFile(data.file_url);
+            const invalidReg = /[\\/:\*\?"\<\>\|]/g;
+            const ext = data.filename.substring(data.filename.lastIndexOf("."));
+            const filename = `${data.title.replace(invalidReg, " ")}${ext}`;
+            if (session.guild) {
+              const gid = session.event.guild.id;
+              await session.onebot
+                .uploadGroupFile(gid, path, filename)
+                .catch(logger.error);
+            } else {
+              const uid = session.event.user.id;
+              await session.onebot
+                .uploadPrivateFile(uid, path, filename)
+                .catch(logger.error);
+            }
+          } else {
+            await session.send([h.file(data.file_url)]);
           }
         }
-      } else {
-        session.send([h.quote(id), h.text("不正确的链接格式")]);
       }
-    });
+    } else {
+      session.send([h.quote(id), h.text(session.text(".invalid_link"))]);
+    }
+  });
 
   ctx.on("message", (session) => {
     if (config.autoRecognise) {
       const text = session.content;
       if (regexp.test(text)) {
-        const cmd = text.replace(/connect/i, "workshop");
-        session.execute(cmd);
+        session.execute(`workshop ${text}`);
       }
     }
   });
